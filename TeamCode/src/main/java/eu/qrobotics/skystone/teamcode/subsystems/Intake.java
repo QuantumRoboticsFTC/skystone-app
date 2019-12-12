01eda874211f791839f267d75cb4ebf6c4d42013
Radu Pogonariu <radu.pogonariu@gmail.com>
2019-12-12 18:53:24 +0200

    Add base intake subsystem

--- TeamCode/src/main/java/eu/qrobotics/skystone/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/eu/qrobotics/skystone/teamcode/subsystems/Intake.java
@@ -1,14 +1,58 @@
 package eu.qrobotics.skystone.teamcode.subsystems;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Intake implements Subsystem {
-    Intake(HardwareMap hardwareMap, Robot robot) {
+import org.openftc.revextensions2.ExpansionHubMotor;
 
+@Config
+public class Intake implements Subsystem {
+
+    public enum IntakeMode {
+        IN,
+        IDLE,
+        OUT,
+        OUT_SLOW
+    }
+
+    public static double INTAKE_IN_SPEED = 0.9;
+    public static double INTAKE_IDLE_SPEED = 0;
+    public static double INTAKE_OUT_SPEED = -0.8;
+    public static double INTAKE_OUT_SLOW_SPEED = -0.4;
+
+    public IntakeMode intakeMode;
+    private ExpansionHubMotor leftIntake, rightIntake;
+    private Robot robot;
+
+    Intake(HardwareMap hardwareMap, Robot robot) {
+        this.robot = robot;
+
+        leftIntake = hardwareMap.get(ExpansionHubMotor.class, "leftIntakeMotor");
+        rightIntake = hardwareMap.get(ExpansionHubMotor.class, "rightIntakeMotor");
+
+        leftIntake.setDirection(DcMotor.Direction.REVERSE);
     }
 
     @Override
     public void update() {
-
+        switch (intakeMode) {
+            case IN:
+                leftIntake.setPower(INTAKE_IN_SPEED);
+                rightIntake.setPower(INTAKE_IN_SPEED);
+                break;
+            case IDLE:
+                leftIntake.setPower(INTAKE_IDLE_SPEED);
+                rightIntake.setPower(INTAKE_IDLE_SPEED);
+                break;
+            case OUT:
+                leftIntake.setPower(INTAKE_OUT_SPEED);
+                rightIntake.setPower(INTAKE_OUT_SPEED);
+                break;
+            case OUT_SLOW:
+                leftIntake.setPower(INTAKE_OUT_SLOW_SPEED);
+                rightIntake.setPower(INTAKE_OUT_SLOW_SPEED);
+                break;
+        }
     }
 }
